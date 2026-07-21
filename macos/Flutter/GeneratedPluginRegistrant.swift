e926fe7cfa20dacc785cf7701f407e9ed243b88c
Nguyen Dieu Vi <nguyendieuvi1230@gmail.com>
2026-07-21 22:30:53 +0700

    Revert auto-generated plugin registrant changes
    These native platform registrant files (linux/macos/windows) are
    regenerated by Flutter tooling itself and don't need to be hand-committed
    alongside the pubspec changes.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,17 +8,13 @@ import Foundation
 import file_picker
 import firebase_auth
 import firebase_core
-import geolocator_apple
 import mobile_scanner
 import shared_preferences_foundation
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
